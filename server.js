08a8f888c42270056693d070f8fce51a66f18b16
Holubinka Vitalik <holybinka-1996@outlook.com>
2019-12-18 02:25:42 +0200

    first comment1234

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/angular-basics'));
 
-app.listen(process.env.PORT || 8090);
+app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req,res) {
 
   res.sendFile(path.join(__dirname+'/dist/angular-basics/index.html'));
 });
 
-console.log('Console listening!')
+console.log('Console listening!');
 // Start the app by listening on the default Heroku port
 
