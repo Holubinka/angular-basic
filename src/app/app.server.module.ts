16e88d1b877d7744e189a89443a1d11685cbfda6
Holubinka Vitalik <holybinka-1996@outlook.com>
2020-01-09 23:20:38 +0200

    final comment

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,14 +3,13 @@ import {AppModule} from './app.module';
 import {ServerModule, ServerTransferStateModule} from '@angular/platform-server';
 import {ModuleMapLoaderModule} from '@nguniversal/module-map-ngfactory-loader';
 import { AppComponent } from './app.component';
-import {FlexLayoutServerModule} from '@angular/flex-layout/server';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
     ModuleMapLoaderModule,
-    FlexLayoutServerModule
+    ServerTransferStateModule,
   ],
   bootstrap: [AppComponent]
 })
