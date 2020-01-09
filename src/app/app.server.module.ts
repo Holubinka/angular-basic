d66478fbe55b06b4775d81ff5f8ddb39b56c559c
holubinka <vitalii.holubinka@crystalogic.net>
2020-01-09 17:16:52 +0200

    deploy application

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,13 +3,14 @@ import {AppModule} from './app.module';
 import {ServerModule, ServerTransferStateModule} from '@angular/platform-server';
 import {ModuleMapLoaderModule} from '@nguniversal/module-map-ngfactory-loader';
 import { AppComponent } from './app.component';
+import {FlexLayoutServerModule} from '@angular/flex-layout/server';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
     ModuleMapLoaderModule,
-    ServerTransferStateModule,
+    FlexLayoutServerModule
   ],
   bootstrap: [AppComponent]
 })
